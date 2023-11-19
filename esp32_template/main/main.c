8703a3b5aa347f4fd837a33924ab0ce807065529
Phong Nguyen <118428350+urboob21@users.noreply.github.com>
2023-11-19 14:55:06 +0700

    [1] RGB LED - rgb led dislay feature (test)

--- esp32_template/main/main.c
+++ esp32_template/main/main.c
@@ -3,16 +3,13 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 #include "sdkconfig.h"
-#include "rgb_led.h"
 #include "esp_event.h"
+#include "rgb_led.h"
 
 void app_main(void) {
 	while (true) {
-		rgb_led_wifi_app_started();
 		vTaskDelay(1000 / portTICK_PERIOD_MS);
-		rgb_led_http_app_started();
 		vTaskDelay(1000 / portTICK_PERIOD_MS);
-		rgb_led_wifi_connected();
 		vTaskDelay(1000 / portTICK_PERIOD_MS);
 	}
 }
--- esp32_template/main/rgb_led.h
+++ esp32_template/main/rgb_led.h
@@ -1,26 +1,35 @@
 /*
  * rgb_led.h
  *
- *  Created on: Oct 28, 2023
+ *  Created on: Nov 19, 2023
  *      Author: Phong Nguyen
  */
 
 #ifndef MAIN_RGB_LED_H_
 #define MAIN_RGB_LED_H_
+// RGB GPIOs LED
+#define RGB_GPIO_LED_BLUE		21
+#define RGB_GPIO_LED_GREEN		22
+#define RGB_GPIO_LED_RED		23
+
+// RGB LED Channels
+#define RGB_CHANNEL_NUM			3
+
+// RGB LED configuration
+typedef struct {
+	int channel;
+	int gpio;
+	int mode;
+	int timer_index;
+} ledc_info_t;
 
 /**
- * Indicate Wifi application has started
+ * Color to indicate
  */
-void rgb_led_wifi_app_started(void);
+void rgb_led_test_started();
 
 /**
- * Indicate HTTP application has started
+ * User R-G-B color
  */
-void rgb_led_http_app_started(void);
-
-/**
- * Indicate connection to an acess point
- */
-void rgb_led_wifi_connected(void);
-
+void rgb_led_display(uint8_t r,uint8_t g,uint8_t b);
 #endif /* MAIN_RGB_LED_H_ */
